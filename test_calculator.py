b19123c27435fcb52cee2784ec9872b90c114fb0
Singh <ShaktiAnand.Singh@lr.org>
2024-10-29 22:30:47 +0530

    Unit test first commit

---
+++ test_calculator.py
@@ -0,0 +1,19 @@
+import pytest
+from calculator import Calculator
+calculator=Calculator()
+
+def test_add():
+    assert calculator.add(2,3)== 5
+
+def test_subtract():
+    assert calculator.subtract(5,4)== 1
+
+def test_multiply():
+    assert calculator.multiply(5,7)== 35
+
+def test_divide():
+    assert calculator.divide(6,2)== 3
+
+def test_divide_by_zero():
+    with pytest(ValueError):
+     calculator.divide(6,0)
